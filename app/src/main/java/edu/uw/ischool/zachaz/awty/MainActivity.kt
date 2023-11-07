97da0e85d6ec8f55cf0eb2fc726b61accc7bde18
freymond <37636251+freymend@users.noreply.github.com>
2023-11-07 10:25:48 -0800

    alarm manager

--- app/src/main/java/edu/uw/ischool/zachaz/awty/MainActivity.kt
+++ app/src/main/java/edu/uw/ischool/zachaz/awty/MainActivity.kt
@@ -1,14 +1,22 @@
 package edu.uw.ischool.zachaz.awty
 
+import android.app.AlarmManager
+import android.app.PendingIntent
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 import android.widget.EditText
 import android.widget.Toast
 import edu.uw.ischool.zachaz.awty.databinding.ActivityMainBinding
 
+const val ALARM_ACTION = "edu.uw.ischool.zachaz.awty.ALARM"
+
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    private var receiver: BroadcastReceiver? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,30 +28,59 @@ class MainActivity : AppCompatActivity() {
         var minutes = binding.minutes.text.toString()
 
 
-        fun validInformation(): Boolean {
-            return !(message.isEmpty() || phoneNumber.isEmpty() || minutes.isEmpty())
-        }
-
         binding.phoneNumber.setOnEditorActionListener { it, _, event ->
-            minutes = (it as EditText).text.toString()
-            event != null
-        }
-        binding.message.setOnEditorActionListener { it, _, event ->
             phoneNumber = (it as EditText).text.toString()
             event != null
         }
-        binding.minutes.setOnEditorActionListener { it, _, event ->
+        binding.message.setOnEditorActionListener { it, _, event ->
             message = (it as EditText).text.toString()
             event != null
         }
+        binding.minutes.setOnEditorActionListener { it, _, event ->
+            minutes = (it as EditText).text.toString()
+            event != null
+        }
 
-        fun sendMessage() : String {
-            Log.i("MainActivity", "Sending message: $message to $phoneNumber")
+        val activityThis = this
+
+        fun sendMessage(): String {
+            if (receiver == null) {
+                receiver = object : BroadcastReceiver() {
+                    override fun onReceive(context: Context?, intent: Intent?) {
+                        Toast.makeText(activityThis, message, Toast.LENGTH_SHORT).show()
+                    }
+                }
+
+                val filter = IntentFilter(ALARM_ACTION)
+                registerReceiver(receiver, filter)
+            }
+
+
+            val intent = Intent(ALARM_ACTION)
+            val pendingIntent =
+                PendingIntent.getBroadcast(this, 0, intent, PendingIntent.FLAG_IMMUTABLE)
+
+            val alarmManager: AlarmManager = getSystemService(Context.ALARM_SERVICE) as AlarmManager
+            alarmManager.setRepeating(
+                AlarmManager.RTC_WAKEUP,
+                System.currentTimeMillis(),
+                minutes.toLong() * 60000,
+                pendingIntent
+            )
+
             return getString(R.string.stop)
         }
-        fun stopMessage() : String {
-            Log.i("MainActivity", "Stopping message: $message to $phoneNumber")
+
+        fun stopMessage(): String {
+            if (receiver != null) {
+                unregisterReceiver(receiver)
+                receiver = null
+            }
             return getString(R.string.start)
+        }
+
+        fun validInformation(): Boolean {
+            return !(message.isEmpty() || phoneNumber.isEmpty() || minutes.isEmpty())
         }
 
         binding.send.setOnClickListener {
@@ -54,7 +91,7 @@ class MainActivity : AppCompatActivity() {
                     else -> stopMessage()
                 }
             } else {
-                Toast.makeText(this, "Please enter all information", Toast.LENGTH_SHORT).show()
+                Toast.makeText(this, "Please fill all the fields", Toast.LENGTH_SHORT).show()
             }
         }
     }
